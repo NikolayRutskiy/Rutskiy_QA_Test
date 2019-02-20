6cfa8f6f6908272f3c301bdc11a525511c9b399b
Nikolay Rutskiy <nrutski@mail.ru>
2019-02-20 12:20:30 +0200

    Add logger and listener

--- second-selenium-project/src/test/java/tests/LoginTest.java
+++ second-selenium-project/src/test/java/tests/LoginTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
+  Logger logger = LoggerFactory.getLogger(LoginTest.class);
 @BeforeMethod
 public void ensurePreconditions() {
   if (app.getSessionHelper().isUserLoggedIn()) {
@@ -12,9 +15,11 @@ public void ensurePreconditions() {
 }
   @Test
   public void testLogIn() {
+  logger.info("start testLogin");
     app.getSessionHelper().clickOnLoginButton();
     app.getSessionHelper().fillLoginForm("elena.telran@yahoo.com", "12345.com");
     app.getSessionHelper().confirmLogin();
+    logger.info("stop testLogin");
     }
 
 
